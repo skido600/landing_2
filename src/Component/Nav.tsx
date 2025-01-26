81fc12acbcfab54d74fbd7d9361a3c748b898714
Chuks <63956312+skido600@users.noreply.github.com>
2025-01-27 00:16:10 +0100

    modify menu

--- src/Component/Nav.tsx
+++ src/Component/Nav.tsx
@@ -24,43 +24,47 @@ const Nav: React.FC = () => {
       </div>
 
       {/* Menu */}
-      <div
-        className={`md:bg-[#6F4436] z-10  bg-[#2F1F14] top-0 right-0 bottom-0 md:relative fixed 
-        md:translate-x-0 px-6 py-2 md:rounded-lg transition-transform duration-300 transform 
-        ${
+      <main
+        className={`fixed   ${
           toggle ? "translate-x-0 " : "translate-x-full"
-        } md:space-y-0 space-y-8  lg:ml-[8rem] `}
+        }  md:translate-x-0  z-10 transition-transform duration-300 transform  top-0 right-0 bottom-0 md:relative bg-[#06141799] backdrop-blur-sm w-full`}
       >
-        {/* Close Icon */}
-        <div className="md:hidden" onClick={handleToggle}>
-          <IoMdClose
-            size={40}
-            className="float-right text-[#6F4436] mb-11 cursor-pointer"
-          />
-        </div>
+        <div
+          className={`md:bg-[#6F4436]   bg-[#2F1F14] top-0 right-0 bottom-0 md:relative fixed 
+      px-6 py-2 md:rounded-lg 
+        md:space-y-0 space-y-8  lg:ml-[8rem] `}
+        >
+          {/* Close Icon */}
+          <div className="md:hidden" onClick={handleToggle}>
+            <IoMdClose
+              size={40}
+              className="float-right text-[#6F4436] mb-11 cursor-pointer"
+            />
+          </div>
 
-        {/* Navigation Links */}
-        <ul className="md:flex gap-x-8 py-20  md:h-auto  h-screen space-y-7 md:space-y-0 md:py-0 items-center md:text-[13px] text-[30px] text-white font-inter">
-          {navItems.map((item, index) => (
-            <li
-              key={index}
-              onClick={() => handleClick(index)}
-              className={`cursor-pointer ${
-                activeIndex === index
-                  ? "bg-white text-[#2F1F14] px-4 md:px-4 md:py-1 rounded-lg transition-all duration-300"
-                  : ""
-              }`}
-            >
-              {item}
-              {activeIndex === index && (
-                <span className="ml-2 sm:hidden text-[#6F4436] text-[30px]">
-                  -
-                </span>
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
+          {/* Navigation Links */}
+          <ul className="md:flex gap-x-8 py-20  md:h-auto  h-screen space-y-7 md:space-y-0 md:py-0 items-center md:text-[13px] text-[30px] text-white font-inter">
+            {navItems.map((item, index) => (
+              <li
+                key={index}
+                onClick={() => handleClick(index)}
+                className={`cursor-pointer ${
+                  activeIndex === index
+                    ? "bg-white text-[#2F1F14] px-4 md:px-4 md:py-1 rounded-lg transition-all duration-300"
+                    : ""
+                }`}
+              >
+                {item}
+                {activeIndex === index && (
+                  <span className="ml-2 sm:hidden text-[#6F4436] text-[30px]">
+                    -
+                  </span>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
 
       {/* Hamburger Icon */}
       <div className="md:hidden" onClick={handleToggle}>
